5791d2876ecb131bce1ed78b766cd39d010a14c9
joseant8 <j2a2mari@gmail.com>
2021-07-29 13:16:38 +0200

    add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './pages/LoginPage';
+import DashBoardPage from './pages/DashBoardPage';
+import LifeCycle from './components/LifeCycle';
+
+// TODO Pasarlo a SessionStorage.
+// Variable para controlar las rutas de la app
+let loggedIn = true;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          { loggedIn ?
+            <Redirect from='/' to='/dashboard'></Redirect>
+            :
+            <Redirect from='/' to='/login'></Redirect>
+          }
+        </Route>
+        <Route exact path='/login' component={LoginPage}>
+
+        </Route>
+        <Route exact path='/dashboard'>
+          { loggedIn ?
+            <DashBoardPage></DashBoardPage>
+            :
+            <Redirect from='/dashboard' to='/login'></Redirect>
+          }
+        </Route>
+        <Route exact path='/lifecycle' component={LifeCycle} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
